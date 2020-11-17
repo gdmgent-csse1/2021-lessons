0ed4243cf15a055757cae24d52dc99da62235328
Kristof Raes <krisra@arteveldehs.be>
2020-11-17 10:55:53 +0100

    WIP

--- Lessen/Les8/Oefening3/Persoon.cs
+++ Lessen/Les8/Oefening3/Persoon.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 public class Persoon : ILogging
 {
@@ -8,6 +9,13 @@ public class Persoon : ILogging
     private List<Adres> adressen;
 
     public Persoon(string naam, string voornaam, string email, Adres adres)
+    {
+        this.naam = naam;
+        this.voornaam = voornaam;
+        this.priveMail = email;
+    }
+
+    public Persoon(string naam, string voornaam, string email)
     {
         this.naam = naam;
         this.voornaam = voornaam;
